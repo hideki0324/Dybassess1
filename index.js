c0694d713473f036226d2fe2e648b26680de0828
hideki0324 <hideki19850324@gmail.com>
2018-02-21 23:22:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.getElementById("bg1").addEventListener("click", function(){
+    document.getElementById("ch1").style.backgroundImage = "url("imgs/i1.jpg")";
+    document.getElementById("ch2").style.backgroundImage = "url("imgs/i2.jpg")";
+    document.getElementById("ch3").style.backgroundImage = "url("imgs/i3.jpg")";
+});
+
+document.getElementById("bg2").addEventListener("click", function(){
+    document.getElementById("ch1").style.backgroundImage = "url("imgs/i4.jpg")";
+    document.getElementById("ch2").style.backgroundImage = "url("imgs/i5.jpg")";
+    document.getElementById("ch3").style.backgroundImage = "url("imgs/i6.jpg")";
+});
+
+document.getElementById("bg3").addEventListener("click", function(){
+    document.getElementById("ch1").style.backgroundImage = "url("imgs/i7.jpg")";
+    document.getElementById("ch2").style.backgroundImage = "url("imgs/i8.jpg")";
+    document.getElementById("ch3").style.backgroundImage = "url("imgs/i9.jpg")";
+});
+
+document.getElementById("bg4").addEventListener("click", function(){
+    document.getElementById("ch1").style.backgroundImage = "url("imgs/i10.jpg")";
+    document.getElementById("ch2").style.backgroundImage = "url("imgs/i11.jpg")";
+    document.getElementById("ch3").style.backgroundImage = "url("imgs/i12.jpg")";
+});
+
